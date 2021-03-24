30c2557cb6f438edace413c73bd71a579d8f11ac
elegant-codes <ajakayetolu@gmail.com>
2021-03-24 14:17:17 +0100

    first commits

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+(function(){
+/* ====================
+    Preloader
+    ======================= */
+    window.onload = function () {
+      window.setTimeout(fadeout, 1000);
+    }
+  
+    function fadeout() {
+      document.querySelector('.preloader').style.opacity = '0';
+      document.querySelector('.preloader').style.display = 'none';
+    }
+})()
